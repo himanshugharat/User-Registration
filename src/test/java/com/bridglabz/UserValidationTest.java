28ddebd994ec5a543835c6ed30cf16c0bab30474
unknown <himanshugharat44@gmail.com>
2020-08-11 03:43:06 -0500

    ADDED:now we validate the first name

--- src/test/java/com/bridglabz/UserValidationTest.java
+++ src/test/java/com/bridglabz/UserValidationTest.java
@@ -1,5 +1,21 @@
 package com.bridglabz;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class UserValidationTest {
-    
+    @Test
+    public void givenFirstName_WhenProper_ShouldReturnTrue() {
+        UserValidation userValidation=new UserValidation();
+        boolean result=userValidation.validateFirstName("Himanshu");
+        Assert.assertEquals(true,result);
+    }
+
+    @Test
+    public void givenFirstName_WhenShort_ShouldReturnFalse() {
+        UserValidation userValidation=new UserValidation();
+        boolean result=userValidation.validateFirstName("Hi");
+        Assert.assertEquals(false,result);
+
+    }
 }
